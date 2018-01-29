a8cc78d8f8ce4b62ef3f38ff8a9b5a328c1bd4ef
Sonny Martin <sonny.martin@diffblue.com>
2018-01-29 14:58:41 +0000

    Adds utility files that assist in creating a release

---
+++ release_util_script_encrypt_var.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+SSL_PWD="$1"
+VAR="$2"
+
+echo "encrypted var is:"
+VAR_ENC=`echo "${VAR}" | openssl aes-256-cbc -a -salt -pass pass:${SSL_PWD} | openssl enc -A -base64`
+echo ${VAR_ENC}
