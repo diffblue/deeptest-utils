05a2a224bc210f5fc2bb9c091c768b8e408d5bb4
thk123 <thomas@diffblue.com>
2018-07-02 17:24:45 +0100

    Adding script to verify the pom version matches the changelog

---
+++ check_version.sh
@@ -0,0 +1,10 @@
+changelog_version=$(head -n 1 CHANGELOG.md)
+maven_version=$(mvn help:evaluate -Dexpression=project.version | grep -v -e "^\\[")
+
+if [ "$maven_version" == "$changelog_version" ]
+then
+  exit 0
+fi
+
+echo Version mismatch CHANGELOG.md: $changelog_version vs. Maven: $maven_version
+exit 1
