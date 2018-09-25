b9c9ad564397c1d52726cf4c4429b6eefc27d41c
thk123 <thomas@diffblue.com>
2018-09-25 15:17:49 +0100

    Amending the script

--- check_version.sh
+++ check_version.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 changelog_version=$(head -n 1 CHANGELOG.md)
 maven_version=$(mvn help:evaluate -Dexpression=project.version | grep -v -e "^\\[")
 
@@ -6,5 +8,5 @@ then
   exit 0
 fi
 
-echo Version mismatch CHANGELOG.md: $changelog_version vs. Maven: $maven_version
+echo Version mismatch CHANGELOG.md: "$changelog_version" vs. Maven: "$maven_version"
 exit 1
